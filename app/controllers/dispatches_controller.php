fce2f101f17d0b4dbb9c116d711eadc514b1c1c0
Don Drake <ddrake@dreamingmind.com>
2011-10-22 22:33:18 -0700

    cleanup prep for new direction with dispatch

--- app/controllers/dispatches_controller.php
+++ app/controllers/dispatches_controller.php
@@ -19,6 +19,36 @@ class DispatchesController extends AppController {
 		);
 	}
 */
+        
+    /**
+     * Action:on_the_bench is the main visitor landing page for news feeds
+     * 
+     * Use a gallery name + bloggish date urls (yyyy/mm)
+     *  (eg: dispatch/all/2011/11; dispatch/journals/2011/03)
+     * Make it admin aware so update tools are available right on
+     *  the normal page but are surpressable/invocable (javascript/div visibility)
+     * Display content in reverse chronological, scrolling; but chunked in 
+     *  pages that match the thumbnail nav pages that are also present.
+     *  A 'more' button at the end of the scrolling list mirrors the 'next'
+     *  page link. There is no 'previous' equivalent in the scrolling list.
+     *  *** maybe older/newer for the scolling list
+     * Individual entries must have anchors so the thumbnail clicks can 
+     *  jump to them.
+     * The thumbnail nav bar should have next/prev page, next/prev image
+     *  (and that should auto-turn pages) and possibly a slideshow that uses
+     *  javascript to scroll to each anchor in turn
+     * A search box should only search dispatches (present results in 
+     *  Action:dispatch_search) and will return gallery-name, text/alt/title
+     *  results in 2 different results
+     * 
+     * @param string $gallery gallery name to fetch or 'all'
+     * @param string $year year of the records to fetch
+     * @param string $month month of the records to fetch
+     * @param string $page page of records to fetch
+     */
+    function on_the_bench($gallery='all', $year=null, $month=null, $page=null){
+        //$gallery='all', $year=date('Y'), $month=date('m'), $page=null
+    }
 
         /**
          * Build the starter arrays of table data for bulk image processing
@@ -223,19 +253,31 @@ class DispatchesController extends AppController {
         }
 
         /**
-         * Return of input form fragment via Ajax to allow data entry for a new image
+         * Return of input form fragment via Ajax to allow data entry for a new image or save the data
+         * 
+         * If data is empty, return a form for the user to enter data
+         * Otherwise:
+         * Restructure the data so it can be saved.
+         * Multiple record requests come in as
+         *   [field_name1]
+         *       [record1 data]
+         *       [record2 data]
+         *   [field_name2]
+         *       [record1 data]
+         *       [record2 data]
+         * The loop turns this into a normal Cake data array
          */
         function new_image_record(){
             if(!isset($this->data)){
                 $this->layout = '';
                 $this->set('path', WWW_ROOT.'img'.DS.'uploads'.DS.'dispatches'.DS);
-                $this->data = $this->Dispatch->find('all', array('conditions'=>array('img_file'=> $this->params['pass'][0])));
+                $this->data = $this->Dispatch->find('all', array('conditions'=>array('Dispatch.img_file'=> $this->params['pass'][0])));
                 $this->render('new_image_record', 'ajax');
             } else {
                 $fields = array_keys($this->data);
                 $img_file = array_keys($this->data['img_file']);
                 $i =0;
-                for($i; $i < count($this->data['id']); $i++) {
+                for($i; $i < count($this->data['news_text']); $i++) {
                     foreach($fields as $field){
                         if ($field === 'img_file'){
                             $file = new File($this->data[$field]['tmp_name'][$i]);
@@ -245,7 +287,7 @@ class DispatchesController extends AppController {
                                 $this->data[$field]['error'][$i] = 1;
                             }
                             foreach($img_file as $sub_key){
-                                $data['Dispatch'][$i]['img_file'][$sub_key] = $this->data[$field][$sub_key][$i];
+                                $data['Image'][$i]['img_file'][$sub_key] = $this->data[$field][$sub_key][$i];
                             }
                         } else {
                         $data['Dispatch'][$i][$field] = $this->data[$field][$i];
@@ -256,7 +298,7 @@ class DispatchesController extends AppController {
 //                foreach($data as $dispatch){
 //                    debug($dispatch); die;
 //                    $this->Dispatch->create();
-                    $this->Dispatch->saveAll($data['Dispatch']);
+                    $this->Dispatch->saveAll($data['Dispatch']); //undefined variable 'data'
                     //$this->Session->setFlash('Save failed');
                     //debug($this->Dispatch->validationErrors);
                 //}
