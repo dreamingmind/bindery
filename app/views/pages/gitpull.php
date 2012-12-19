ea1033a380691c9c43f66a0dc480f6486a9f11c7
Don Drake <ddrake@dreamingmind.com>
2012-12-18 23:00:51 -0800

    more changes
    more

--- app/views/pages/gitpull.php
+++
@@ -1,7 +0,0 @@
-<?php
-`git pull`
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
--- app/views/workshops/upcoming.ctp
+++ app/views/workshops/upcoming.ctp
@@ -1,5 +1,4 @@
 <h1>Upcoming workshops</h1>
-<h2>Another heading</h2>
     <?php 
 echo $upcoming.'</ br>';
 debug($sessions);
