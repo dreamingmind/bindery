92b5aaf301a323be6fd2b11d152f5824016ad811
Jason Tempestini <jason.tempestini@inframesh.com>
2012-12-27 12:41:35 -0800

    Changes to Workshop model

--- app/models/workshop.php
+++ app/models/workshop.php
@@ -40,77 +40,171 @@ class Workshop extends AppModel {
 			'exclusive' => '',
 			'finderQuery' => '',
 			'counterQuery' => ''
-		)
+		),
+                'ContentCollection'
 	);
         
+//        /**
+//         * @var array $upcoming_sessions sessions records, now and future plus related date data
+//         */
+//        var $upcoming_sessions = array();
+//
+//        /**
+//         * @var array $previous_sessions sessions records, all workshops not in upcoming_sessions
+//         */
+//        var $previous_sessions = array();
+//        /**
+//         * @var array $workshops array of all workshop data (no related data) indexed by workshop_id
+//         */
+//        var $workshops = array();
         /**
-         * @var array $upcoming_sessions sessions records, now and future plus related date data
+         * @var array $workshops_all array of all workshop data
+         * contains all related content and images
+         * contains current and future sessions and dates
          */
-        var $upcoming_sessions = array();
+        var $workshops_all = array();
+        /**
+         * @var array $workshops_potential array of workshops_all that didn't return sessions
+         */
+        var $workshops_potential = array();
+        /**
+         * @var array $workshops_upcoming contains the members of workshops_all array
+         * containing only those with sessions with dates in the future
+         */
+        var $workshops_upcoming = array();
+        /**
+         * @var array $workshops_now contains the members of workshops_all array
+         * containing only those with sessions where the date is today
+         */
+        var $workshops_now = array();
 
-        /**
-         * @var array $previous_sessions sessions records, all workshops not in upcoming_sessions
-         */
-        var $previous_sessions = array();
 
-        
-        /**
-         * @var array $workshops array of all workshop data (no related data) indexed by workshop_id
-         */
-        var $workshops = array();
         
         function __construct() {
             parent::__construct();
-            $this->upcomingSessions();
-            $this->getWorkshops();
-            $this->previousSessions();
+//            $this->upcomingSessions();
+//            $this->getWorkshops();
+//            $this->previousSessions();
+            $this->workshopsAll();
 //            debug($this->workshops);die;
         }
 
+//        /**
+//         * Set the models $upcoming_session property
+//         * 
+//         * Sessions where last_day>=current date sorted ascending
+//         * Includes related date data
+//         */
+//        function upcomingSessions(){
+//            $this->upcoming_sessions = $this->Session->find('all', array(
+//                'conditions'=>'Session.last_day > CURDATE()',
+//                'order'=>'Session.first_day ASC',
+////                'fields'=> array('workshop_id','title','cost','participants','first_day','last_day'),
+//                'contain'=>array('Date','Workshop')
+//            ));
+//        }
+//        
+//        /**
+//         * Set the models $workshops property
+//         * 
+//         * Pull all workshop data into an id indexed array. No related data
+//         */
+//        function getWorkshops() {
+//            $temp_workshops = $this->find('all',array(
+//                'conditions' => array(
+//                    'category_id' => $this->Category->categoryNI['workshop']),
+//                'recursive'=>0
+//                ));
+//            $this->workshops = array();
+//            foreach($temp_workshops as $val) {
+//                $this->workshops[$val['Workshop']['id']] = $val;
+//            }
+//        }
+//        
+//        /**
+//         * Set the models $previous_session property
+//         * 
+//         * Set $pervious_sessions to and id indexed array of workshops
+//         * that have no future session scheduled (no related data
+//         */
+//        function previousSessions(){
+//            $this->previous_sessions = $this->workshops;
+//            foreach($this->upcoming_sessions as $val) {
+//                unset($this->previous_sessions[$val['Workshop']['id']]);
+//            }
+//        }
         /**
          * Set the models $upcoming_session property
          * 
          * Sessions where last_day>=current date sorted ascending
          * Includes related date data
          */
-        function upcomingSessions(){
-            $this->upcoming_sessions = $this->Session->find('all', array(
-                'conditions'=>'Session.last_day > CURDATE()',
-                'order'=>'Session.first_day ASC',
-//                'fields'=> array('workshop_id','title','cost','participants','first_day','last_day'),
-                'contain'=>array('Date','Workshop')
-            ));
-        }
-        
-        /**
-         * Set the models $workshops property
-         * 
-         * Pull all workshop data into an id indexed array. No related data
-         */
-        function getWorkshops() {
-            $temp_workshops = $this->find('all',array(
-                'conditions' => array(
-                    'category_id' => $this->Category->categoryNI['workshop']),
-                'recursive'=>0
-                ));
-            $this->workshops = array();
-            foreach($temp_workshops as $val) {
-                $this->workshops[$val['Workshop']['id']] = $val;
-            }
-        }
-        
-        /**
-         * Set the models $previous_session property
-         * 
-         * Set $pervious_sessions to and id indexed array of workshops
-         * that have no future session scheduled (no related data
-         */
-        function previousSessions(){
-            $this->previous_sessions = $this->workshops;
-            foreach($this->upcoming_sessions as $val) {
-                unset($this->previous_sessions[$val['Workshop']['id']]);
-            }
-        }
+        function workshopsAll(){
+            $this->workshops_all=
+            $this->find('all', array(
+                'conditions'=>array(
+                    'Workshop.category_id'=>$this->Category->categoryNI['workshop'],
+                    'Workshop.publish'=>1
+                ),
+                'order'=>'Workshop.heading',
+                'fields'=> array('id','heading','text','role','category_id','slug'),
+                'contain'=>array(
+                    'ContentCollection'=>array(
+                        'fields'=>array(
+                            'ContentCollection.content_id',
+                            'ContentCollection.collection_id',
+                            'Content'=>array(
+                                'fields'=>array(
+                                    'Content.id',
+                                    'Content.content',
+                                    'Content.image_id',
+                                    'Content.alt',
+                                    'Content.title',
+                                    'Content.heading',
+                                    'Content.slug',
+                                    'Content.publish',
+                                    'Image'=>array(
+                                        'fields'=>array(
+                                            'Image.img_file',
+                                            'Image.id',
+                                            'Image.title',
+                                            'Image.alt'
+                                        )
+                                    )
+                                ),
+                                'conditions'=>array(
+                                    'Content.publish'=>1
+                                )
+                            )
+                        )
+                    ),
+                    'Session'=>array(
+                        'fields'=>array(
+                            'Session.id',
+                            'Session.collection_id',
+                            'Session.cost',
+                            'Session.participants',
+                            'Session.first_day',
+                            'Session.last_day',
+                            'Session.registered',
+                            'Date'=>array(
+                                'fields'=>array(
+                                    'Date.session_id',
+                                    'Date.date',
+                                    'Date.start_time',
+                                    'Date.end_time'
+                                )
+                            )
+                        ),
+                        'conditions'=>array(
+                            'Session.last_day >= CURDATE()'
+                        )
+                    )
+                )
+            )
+        );
+       debug($this->workshops_all);die;
+       }
 
 }
 ?>
