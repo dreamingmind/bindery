e9ac1de4a25870b220e530816c6659825f893e6e
Don Drake <ddrake@dreamingmind.com>
2013-06-09 22:43:54 -0700

    modify purchase grid show/hide
    change from display to visibility so the table keeps its shape and chaos is minimized

--- app/webroot/js/catalog.js
+++ app/webroot/js/catalog.js
@@ -3,27 +3,11 @@ $(document).ready(function(){
     // initials filter checkboxes
     $('.filters input[type="checkbox"]').attr('checked','checked').bind('change', function(){
         if($(this).attr('checked')){
-            $('.'+$(this).attr('id')).css('display','table-cell');
+            $('.'+$(this).attr('id')).css('visibility','visible');
         } else {
-            $('.'+$(this).attr('id')).css('display','none');
+            $('.'+$(this).attr('id')).css('visibility','collapse');
         }
-          manageXXandYY();
+//          manageXXandYY();
     });
-    
-    function manageXXandYY(){
-        var columns = $('.xx');
-        columns.each(function(){
-//            alert($(this).attr('class').replace('xx', '').replace(' ', ''));
-            var childColumns = $('.x[style="display: table-cell;"]').filter($(this).attr('class').replace('xx', '').replace(' ', ''));
-//            $('.x.'+$(this).attr('class')+'[style="dispaly: table-cell"]'.replace('xx', ''));
-//            alert('columns'+childColumns.length);
-        });
-        var rows = $('.yy');
-        rows.each(function(){
-//            alert($(this).attr('class').replace('yy', '').replace(' ', ''));
-            childColumns = $('.y[style="display: table-cell;"]').filter($(this).attr('class').replace('yy', '').replace(' ', ''));
-//            alert('rows '+childColumns.length);
-        });
-    }
-    
+        
 }) 
