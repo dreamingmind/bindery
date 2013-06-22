e1e308a6f03facfe305d41b6c1f51dcde7dcb125
Don Drake <ddrake@dreamingmind.com>
2013-06-21 21:03:37 -0700

    make purchase tables collapse on startup with javascript

--- app/webroot/js/catalog.js
+++ app/webroot/js/catalog.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
+
+    function initCheckboxes(){
+        // initials filter checkboxes
+        $('.filters input[type="checkbox"]').attr('checked','checked').bind('change', function(){
+            var product = $(this).attr('category');
+    //        alert(product);
+            if($(this).attr('checked')){
+                setting = 1;
+            } else {
+                setting = .35;
+            }
+    //            $('.'+$(this).attr('id')).css('opacity',setting);
+                $('.'+product+'.'+$(this).attr('id')).css('opacity',setting);
+        });
+    }
     
-    // initials filter checkboxes
-    $('.filters input[type="checkbox"]').attr('checked','checked').bind('change', function(){
-        var product = $(this).attr('category');
-//        alert(product);
-        if($(this).attr('checked')){
-            setting = 1;
-        } else {
-            setting = .35;
-        }
-//            $('.'+$(this).attr('id')).css('opacity',setting);
-            $('.'+product+'.'+$(this).attr('id')).css('opacity',setting);
+    function initTableToggleHooks(){
+        $('table').each(function(){
+            var id = $(this).find('tr[class="table"] > td').attr('id');
+            $(this).find('tr[class!="table"]').attr('class',id);
+        })
+    }
+    
+    initCheckboxes();
+    initTableToggleHooks();
+    $('td[class~="table_name"]').each(function(){
+        $('.'+$(this).attr('id')).toggle(function(){
+            
+        });
     });
-    
-    $('table').each(function(){
-        var id = $(this).find('tr[class="table"] > td').attr('id');
-        $(this).find('tr[class!="table"]').attr('class',id);
-    })
 })
