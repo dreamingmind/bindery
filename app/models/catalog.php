c7006e0b2529c35aa5e74effc465c6e38214443f
Don Drake <ddrake@dreamingmind.com>
2013-07-01 07:49:30 -0700

    build model to support option-set reveals on product grid pages
    Just a simple array rather than a big table. I can expand this later if necessary.

--- app/models/catalog.php
+++ app/models/catalog.php
@@ -51,6 +51,41 @@ class Catalog extends AppModel {
 			'order' => ''
                 )
 	);
+        
+        /**
+         * Return the list of setlist attribute values for every product category
+         * 
+         * These will be the option inputs to reveal when a given product category
+         * is clicked in the product grid interface 
+         * 
+         * @return array The array of options indexed by product category
+         */
+        function getAllProductCategoryOptions(){
+            $allOptions = array(
+                'bookbody' => 'ruling',
+                'journals' => 'belt titling casematerials endpapers special',
+                'notebooks' => 'belt titling casematerials linermaterials notbookpockets special',
+                'portfolios' => 'belt titling casematerials linermaterials portfoliopocket special'
+            );
+            
+            return $allOptions;
+        }
 
+        /**
+         * Return a single setlit attribute string
+         * 
+         * @param string $category 
+         * @return string The setlist attribute string for the given category
+         */
+        function getProductCategoryOptions($category){
+            $allOptions = $this->getAllProductCategoryOptions();
+            $result = array();
+            
+            if(isset($allOptions[$category])){
+                $result = $allOptions[$category];
+            }
+            
+            return $result;
+        }
 }
 ?>
