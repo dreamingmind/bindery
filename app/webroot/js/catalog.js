e3379687de99ee8a63d42a3a9f2681ed444918d0
Don Drake <ddrake@dreamingmind.com>
2013-07-26 14:08:12 -0700

    Decompose the new diagram code
    pull out all the primitives. Some went to app.'s
    
    Next, make a setDiagramDefaults() method.
    
    Then setDiagramCss() method.

--- app/webroot/js/catalog.js
+++ app/webroot/js/catalog.js
@@ -39,37 +39,6 @@ Top/Bot margins = '+params.margin.height+' and 2*margin='+params.margin.height*2
     }
     
     /**
-     * Scale a number by a factor
-     * 
-     * If round = true return an integer
-     * otherwise return a float
-     */
-    function scaleTo(original, factor, round){
-        if(typeof(round)==='undefined') round=true;
-        if (round) {
-            return parseInt(original * factor);
-        } else {
-            return original * factor;
-        }
-    }
-    
-    /**
-     * Return an assembled size ojbect
-     */
-    function size(obj, width, height){
-        obj['width'] = width;
-        obj['height'] = height;
-    }
-    
-    /**
-     * return an assembled point object
-     */
-    function point(obj, x, y){
-        obj['x'] = x;
-        obj['y'] = y;
-    }
-    
-    /**
      * calc the diagram div's baseline parameters
      * 
      * Set the margin and offset starting % values
@@ -77,11 +46,48 @@ Top/Bot margins = '+params.margin.height+' and 2*margin='+params.margin.height*2
      * Assumes the first layer is going to be the largest
      */
     function setDiagramParams(params){
-        
+
         // establish some baseline control relationships
         params['offsetMinPercent'] = .1; // layer offset is least 10% of the container div's narrow dimension
         params['offsetMaxPercent'] = .75; // layer offset is at most 75% of the layer's size
         params['marginPercent'] = .08; // margin is 3%
+        
+        // work out divSize, margin, liveArea
+        setDiagramWrapper(params)
+
+        // work out:
+        // limitDirection, looseDirection ('width' or 'height')
+        // limitCoordinate, looseCoordinate ('x' or 'y')
+        findScaleLimit(params)
+        
+        // calulate limit direction offset
+        // widthOffset, widthOffsetTotal or heightOffset, heightOffsetTotal
+        setLimitOffset(params);
+
+        // scale
+        setScaleFactor(params);
+        
+        // add size properties to each layer and each json entry
+        // params.layerSizes.--layerName--.size.width and blah.size.height
+        // diagramData.--targetProperty--.--layerName--.size.width and blah.size.height
+        scaleEachLayer(params);
+        
+        // calulate loose direction offset
+        // widthOffset, widthOffsetTotal or heightOffset, heightOffsetTotal
+        setLooseOffset(params);
+        
+        // Center the diagram in the loose direction
+        adjustLooseMargin(params);        
+    }
+    
+    /**
+     * Set outermost parameters for the diagram wrapper div
+     * 
+     * divSize
+     * margin
+     * liveArea
+     **/
+    function setDiagramWrapper(params){
         
         // create a divSize object, the diagram wrapper div
         params.divSize = new Object();
@@ -100,7 +106,33 @@ Top/Bot margins = '+params.margin.height+' and 2*margin='+params.margin.height*2
         // set the live-area size for the div
         params.liveArea = new Object();
         size(params.liveArea, params.divSize.width - (params.margin.width * 2), params.divSize.height - (params.margin.height * 2))
-        
+    }
+    
+    /**
+     * Adjust the margin in the loose direction to center the diagram
+     */
+    function adjustLooseMargin(params){
+        // the loose direction may not reach from margin to margin
+        // if that is the case, adjust the margins to center things
+        if (params.layerCount > 1) {
+            var content = params[params.looseDirection+'OffsetTotal'] + params.layerSizes[params.layerNames[0]]['size'][params.looseDirection];
+        } else {
+            var content = params.layerSizes[params.layerNames[0]]['size'][params.looseDirection];
+        }
+        var remainder = params.liveArea[params.looseDirection] - content;
+        if (remainder > 0) {
+            remainder = parseInt(remainder/2);
+            params.margin[params.looseDirection] += remainder;
+        }
+    }
+    
+    /**
+     * Work out which dimension limits the layer size
+     * 
+     * Create a set of properties that will act as indexes
+     * into other properties during later work.
+     */
+    function findScaleLimit(params){
         // determine the limiting dimension for scaling the layers
         // one direction (the true one) will have minimum offsets
         // and the other will have larger offests (up to offsetMax)
@@ -120,7 +152,12 @@ Top/Bot margins = '+params.margin.height+' and 2*margin='+params.margin.height*2
             params.limitCoordinate = 'y';
             params.looseCoordinate = 'x';
         }
-        
+    }
+
+    /**
+     * workout the offset and total offset for the limit direction
+     */
+    function setLimitOffset(params){
         // caluculate the pixels used by offsetting in the limiting direction
         // work out both single offest and total offset
         if (params.layerCount > 1) {
@@ -130,11 +167,38 @@ Top/Bot margins = '+params.margin.height+' and 2*margin='+params.margin.height*2
             params[params.limitDirection+'Offset'] = 0;
             params[params.limitDirection+'OffsetTotal'] = 0;
         }
-        
-        // calculate the factor to use when scaling the layers
+    }
+
+    /**
+     * workout the offset and total offset for the limit direction
+     */
+     function setLooseOffset(params){
+        // now work out the loose direcion offset values
+        var looseSize = params.layerSizes[params.layerNames[0]]['size'][params.looseDirection];
+        if (params.layerCount > 1) {
+            params[params.looseDirection+'OffsetTotal'] = params.liveArea[params.looseDirection] - looseSize;
+            params[params.looseDirection+'Offset'] = params[params.looseDirection+'OffsetTotal'] / (params.layerCount - 1);        
+        } else {
+            var remainder = params.liveArea[params.looseDirection] - looseSize;
+            params[params.looseDirection+'Offset'] = remainder;
+            params[params.looseDirection+'OffsetTotal'] = remainder;
+        }
+     }
+     
+    /**
+     * Determine the scale for all layer components
+     */
+    function setScaleFactor(params){
         params.scale = new Object();
         params.scale = (params.liveArea[params.limitDirection] - params[params.limitDirection+'OffsetTotal']) / parseFloat(diagramData[params.targetProduct][params.layerNames[0]][params.limitCoordinate]);
-        
+    }
+    
+    /**
+     * Scale each layer
+     * 
+     * store properties in both params and diagramData (the json object)
+     */
+    function scaleEachLayer(params){
         // work through all the layers and set each one's actual size
         params.layerSizes = new Object();
         var count = 0;
@@ -149,33 +213,37 @@ Top/Bot margins = '+params.margin.height+' and 2*margin='+params.margin.height*2
             diagramData[params.targetProduct][params.layerNames[count]]['size'] = pixelSize;
             count++;
         }
-        
-        // now work out the loose direcion offset values
-        var looseSize = params.layerSizes[params.layerNames[0]]['size'][params.looseDirection];
-        if (params.layerCount > 1) {
-            params[params.looseDirection+'OffsetTotal'] = params.liveArea[params.looseDirection] - looseSize;
-            params[params.looseDirection+'Offset'] = params[params.looseDirection+'OffsetTotal'] / (params.layerCount - 1);        
-        } else {
-            var remainder = params.liveArea[params.looseDirection] - looseSize;
-            params[params.looseDirection+'Offset'] = remainder;
-            params[params.looseDirection+'OffsetTotal'] = remainder;
-        }
-        
-        // the loose direction may not reach from margin to margin
-        // if that is the case, adjust the margins to center things
-        if (params.layerCount > 1) {
-            var content = params[params.looseDirection+'OffsetTotal'] + params.layerSizes[params.layerNames[0]]['size'][params.looseDirection];
-        } else {
-            var content = params.layerSizes[params.layerNames[0]]['size'][params.looseDirection];
-        }
-        var remainder = params.liveArea[params.looseDirection] - content;
-        if (remainder > 0) {
-            remainder = parseInt(remainder/2);
-            params.margin[params.looseDirection] += remainder;
-        }
+    }
+
+    function writeLayerCssRule(layerId){
+//        var rule = 
+//'#%s.%s {\n\
+//    position: absolute;\n\
+//    top: %d;\n\
+//    left: %d;\n\
+//    height: %d;\n\
+//    width: %d\n\
+//}';
+        var rule = 
+'#{0}.{1} {\n\
+    position: absolute;\n\
+    top: {2};\n\
+    left: {3};\n\
+    height: {4};\n\
+    width: {5}\n\
+}';
+        alert(rule.format(layerId,'case','journals',9,10,11,12));
         
     }
     
+    /**
+     * Scan json block to see how many diagram layers
+     * 
+     * Server writes data to json diagramData
+     * targetProduct is Diagram.product_group and Catalog.product_group (link)
+     * json array is custom composed by Catalog Model
+     * layers are primary diagramed product surfaces, case, liner, etc.
+     */
     function getDiagramLayers(targetProduct){
         var count = 0;
         var layer = new Array();
