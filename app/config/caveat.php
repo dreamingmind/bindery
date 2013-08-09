7d47301412479ee5b8d2fc0d0372ccb5341f050f
Don Drake <ddrake@dreamingmind.com>
2013-08-08 19:19:00 -0700

    caveat data addition

--- app/config/caveat.php
+++ app/config/caveat.php
@@ -3,11 +3,12 @@
 /**
  * This is a repository of general alerts, warnings, and messages
  */
-        $config['caveat.pricingNormal'] = 'This price is an estimate and does not include CA sales tax or shipping.';
-        $config['caveat.pricingBallPark'] = 'This price should be considered a \'ball park\' estimate. Some options could not be priced exactly.';
-        $config['caveat.pricingLowBallPark'] = 'This price should be considered a \'low ball park\' estimate. Some options were not included in the price.';
-        $config['caveat.materials'] = 'Leather is a natural product. Every piece has a slightly different texture and color and every computer monitor will display slightly different colors.';
-        $config['caveat.titling'] = 'Titling is difficult to estimate without some discussion. The price added here is for initials or a short name in one location.';
-        $config['caveat.penloop'] = 'Pen loops do not stretch. Size by the diameter of your writing instrument.';
+$config['caveat.Normal'] = 'This price does not include CA sales tax or shipping.';
+$config['caveat.BallPark'] = 'This price should be considered a \'ball park\' estimate. Some options could not be priced exactly.';
+$config['caveat.LowBallPark'] = 'This price should be considered a \'low ball park\' estimate. Some options were not included in the price.';
+$config['caveat.materials'] = 'Leather is a natural product. Every piece has a slightly different texture and color and every computer monitor will display slightly different colors.';
+$config['caveat.titling'] = 'Titling is difficult to estimate without some discussion. The price added here is for initials or a short name in one location.';
+$config['caveat.penloop'] = 'Pen loops do not stretch. Size by the diameter of your writing instrument.';
+$config['caveat.partialBookbody'] = 'There is not enough information to price your Book Body.';
 //        $config['caveat.materials'] = 'Colors are approximate: Leather is a natural product. Every piece has a slightly different texture and color. Additionally, every computer monitor will display slightly different colors.';
-        ?>
+?>
