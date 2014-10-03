ed8cf8da424d683772098ba1ff7ab5e5d82ddf84
Don Drake <ddrake@dreamingmind.com>
2014-10-03 08:45:55 -0700

    Decompose the cart-find process
    closes #198

--- app/Model/Cart.php
+++ app/Model/Cart.php
@@ -2,15 +2,14 @@
 App::uses('AppModel', 'Model');
 /**
  * Cart Model
+ * 
+ * Shopping cart
  *
  * @property User $User
  * @property Session $Session
  * @property Supplement $Supplement
  */
 class Cart extends AppModel {
-
-
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 /**
  * belongsTo associations
@@ -67,19 +66,55 @@ class Cart extends AppModel {
 	 */
 	public function maintain(SessionComponent $Session, $oldSession) {
 		
+		$items = $this->load($Session, $oldSession);
+		
+		if (!empty($items)) {
+//			dmDebug::logVars($items, 'items to transform');
+	//		dmDebug::logVars($this->getLastQuery(), 'Cart->maintain find query for $userId='.$userId);
+			if (is_null($userId)) {
+				$i = Hash::insert($items, '{n}.Cart.phpsession_id', $Session->id());
+				$items = Hash::insert($i, '{n}.Cart.user_id', '');
+			} else {
+				$i = Hash::insert($items, '{n}.Cart.phpsession_id', '');
+				$items = Hash::insert($i, '{n}.Cart.user_id', $userId);
+			}
+//			dmDebug::logVars($items, 'items to save');
+			$this->saveMany($items);
+		}
+	}
+	
+	/**
+	 * Common logic to get the current visitor's cart items
+	 * 
+	 * this->maintenance puts the old session id from the cookie in 
+	 * session id to get the user's last session
+	 * 
+	 * @param object $Session Component or Helper
+	 * @param string $sessionId The session id if the current session is not wanted
+	 * @param boolean $deep Contain related data or not
+	 * @return array {n}.Cart.field
+	 */
+	private function load($Session, $sessionId = FALSE, $deep = FALSE) {
+
 		$userId = $Session->read('Auth.User.id');
-		$itemsUser = array();
+		if (!$sessionId) {
+			$sessionId = $Session->id();
+		}
+		if ($deep) {
+			$contain = array('User', 'Supplement');
+		}
 		
 		$itemsAnon = $this->find('all', array(
 			'conditions' => array(
-				'phpsession_id' => $oldSession,
+				'phpsession_id' => $sessionId,
 				'user_id' => ''
 			),
 			'fields' => array('id', 'phpsession_id', 'user_id'),
-			'contain' => FALSE
+			'contain' => $deep
 		));
 //		dmDebug::logVars($this->getLastQuery(), 'anon find query for $userId='.$userId);
 
+		$itemsUser = array();
 		if (!is_null($userId)) {
 //			dmDebug::logVars($userId, 'user id for query');
 			$itemsUser = $this->find('all', array(
@@ -96,30 +131,10 @@ class Cart extends AppModel {
 //		dmDebug::logVars($itemsAnon, 'anon items');
 //		dmDebug::logVars($itemsUser, 'user items');
 
-		$items = array_merge($itemsAnon, $itemsUser);
-
-		if (!empty($items)) {
-//			dmDebug::logVars($items, 'items to transform');
-	//		dmDebug::logVars($this->getLastQuery(), 'Cart->maintain find query for $userId='.$userId);
-			if (is_null($userId)) {
-				$i = Hash::insert($items, '{n}.Cart.phpsession_id', $Session->id());
-				$items = Hash::insert($i, '{n}.Cart.user_id', '');
-			} else {
-				$i = Hash::insert($items, '{n}.Cart.phpsession_id', '');
-				$items = Hash::insert($i, '{n}.Cart.user_id', $userId);
-			}
-//			dmDebug::logVars($items, 'items to save');
-			$this->saveMany($items);
-		}
+		return array_merge($itemsAnon, $itemsUser);
 	}
-	
-	function getLastQuery()
-    {
-        $dbo = $this->getDatasource();
-        $logs = $dbo->getLog();
-        $lastLog = end($logs['log']);
-        return $lastLog['query'];
-    }
+
+
 	/**
 	 * 
 	 * 
@@ -128,24 +143,7 @@ class Cart extends AppModel {
 	 * @return type
 	 */
 	public function fetch($Session, $deep = FALSE) {
-		$userId = $Session->read('Auth.User.id');
-		
-		$contain = FALSE;
-		$conditions = array('user_id' => $userId);
-		
-		if ($deep) {
-			$contain = array('User', 'Supplement');
-		}
-		if (is_null($userId)) {
-			array('phpsession_id' => $Session->id());
-		}
-		
-		$cart = $this->find('all', array(
-				'conditions' => $conditions,
-				'contain' => $contain
-			));
-		
-		return $cart;
+		return $this->load($Session, FALSE, $deep);
 	}
 
 
