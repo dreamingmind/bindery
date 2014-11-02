23fda5726f09a59273bf8111692de70af2ee405b
Don Drake <ddrake@dreamingmind.com>
2014-11-01 17:42:34 -0700

    missed a bootstap load. fixed now

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -164,7 +164,6 @@ Cache::config('catalog', array(
  */
 	CakePlugin::load('DebugKit');
 	CakePlugin::load('Upload');
-	CakePlugin::load('PaypalIpn');
 	
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By Default CakePHP bundles two filters:
