bcd914b1e2c23913ce2e835a9ac995f619d49422
Don Drake <ddrake@dreamingmind.com>
2015-01-22 09:26:53 -0800

    pending code for wishlist model

--- app/Model/WishList.php
+++ app/Model/WishList.php
@@ -1,11 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 App::uses('AppModel', 'Model');
 
 /**
@@ -14,6 +7,17 @@ App::uses('AppModel', 'Model');
  */
 class WishList extends AppModel {
 	
+//	public $belongsTo = array(
+//		'User' => array(
+//			'className' => 'User',
+//			'foreignKey' => 'user_id',
+////			'counterCache' => 'wish_item_count',
+//			'conditions' => '',
+//			'fields' => '',
+//			'order' => ''
+//		)
+//	);
+
 	public $useTable = 'cart_items';
 	
 }
