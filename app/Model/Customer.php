608de33ec2bb0e48c2fbe9d8533b6ebed4c7fc10
Don Drake <ddrake@dreamingmind.com>
2015-01-22 09:30:53 -0800

    pending customer model code

--- app/Model/Customer.php
+++ app/Model/Customer.php
@@ -1,11 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 App::uses('AppModel', 'Model');
 
 /**
@@ -15,4 +8,21 @@ App::uses('AppModel', 'Model');
 class Customer extends AppModel {
 	
 	public $useTable = 'users';
+	
+//	public $hasMany = array(
+//		'WishList' => array(
+//			'className' => 'WishList',
+//			'foreignKey' => 'user_id',
+//			'dependent' => true,
+//			'conditions' => '',
+//			'fields' => '',
+//			'order' => '',
+//			'limit' => '',
+//			'offset' => '',
+//			'exclusive' => '',
+//			'finderQuery' => '',
+//			'counterQuery' => ''
+//		),
+//	);
+		
 }
