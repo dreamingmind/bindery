4589fc2bfca93981559452dbd7f685e9e54c9523
Jason Tempestini <jason@curlymedia.com>
2015-02-19 12:27:00 -0800

    Naming change

--- app/Plugin/Drawbridge/Model/DrawbridgeAppModel.php
+++ app/Plugin/Drawbridge/Model/DrawbridgeAppModel.php
@@ -1,17 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 App::uses('AppModel', 'Model');
 
 /**
- * CakePHP UserAuthAppModel
+ * CakePHP DrawbridgeAppModel
  * @author jasont
  */
-class UserAuthAppModel extends AppModel {
+class DrawbridgeAppModel extends AppModel {
     
 }
