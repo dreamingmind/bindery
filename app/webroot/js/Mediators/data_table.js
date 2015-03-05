d38e8c166010d0e5041b26d119d835e7c13185bf
Don Drake <ddrake@dreamingmind.com>
2015-03-04 19:43:00 -0800

    noodeling around trying to find my way

--- app/webroot/js/Mediators/data_table.js
+++ app/webroot/js/Mediators/data_table.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	DateTable = new dt({new_call: webroot + 'dates/dateRow',
 		call_for_rows: true
 	});
-	t = new CakeTable_v2_6();
+//	t = new CakeTable_v2_6();
 });
 
 var DateTable = {};
@@ -18,96 +18,89 @@ var DateTable = {};
  *		row will have a save/submit tool (class = .submit) that saves all the data
  */
 function dt(config) {
-	
+
 //	this.init(config);
-	
+
 	// behavioral settings
 	this.field_save = false,
-	this.call_for_rows = false,
-	
-	// callpoints
-	this.new_call = false,
-	this.remove_call = false,
-	this.field_save_call = false,
-	this.submit_call = false,
-	
-	// element selectors
-	this.table_selector = 'table.session_dates > tbody',
-	this.control_row_selector = 'tr.control',
-	this.new_control = 'button.new',
-	this.submit_control = '.submit',
-	
-	// element reference containers
-	this.row_html_template = false,
-	this.control_row = false,
-	this.table = false,
-	this.rows = false,
-	
-	this.scan = function(){
-	},
-	
-	/**
-	 * Configure and initialize the object on page load
-	 */
-	this.init = function(config){
-		if (typeof(config) != 'undefined') {
-			this.configure(config);
-		}
-		$(document).on('mediate', this.scan);
-		this.rows = new record_warehouse('Date');
-		this.table = $(this.table_selector);
-		this.control_row = $(this.control_row_selector);
-		$(this.new_control).on('click',
-		{
-			self: this,
-			call_for_rows: this.call_for_rows,
-			callForNewRow: this.callForNewRow,
-			control_row: this.control_row,
-			rows: this.rows,
-			row_html_template: this.row_html_template
-		}, this.new
-		);
-	},
-	
-	this.new = function(e){
-		if (e.data.self.call_for_rows) {
-			e.data.self.callForNewRow(e);
-			$(e.data.self.control_row).before(e.data.self.rows.add(e.data.self.row_html_template));
-		} else {
-			$(e.data.self.control_row).before(e.data.self.rows.add(e.data.self.row_html_template));
-		}
-	},
-	
-	this.callForNewRow = function(e){
-		$.ajax({
-			type: "GET",
-			dataType: "JSON",
-			url: e.data.self.new_call,
-			async: false,
-			success: function (data) {
-				e.data.self.row_html_template = data.row;
+			this.call_for_rows = false,
+			// callpoints
+			this.new_call = false,
+			this.remove_call = false,
+			this.field_save_call = false,
+			this.submit_call = false,
+			// element selectors
+			this.table_selector = 'table.session_dates > tbody',
+			this.control_row_selector = 'tr.control',
+			this.new_control = 'button.new',
+			this.submit_control = '.submit',
+			// element reference containers
+			this.row_html_template = false,
+			this.control_row = false,
+			this.table = false,
+			this.rows = false,
+			this.scan = function () {
 			},
-			error: function (jqXHR, status, error ) {
-				
-			}
-		})
+			/**
+			 * Configure and initialize the object on page load
+			 */
+			this.init = function (config) {
+				if (typeof (config) != 'undefined') {
+					this.configure(config);
+				}
+				$(document).on('mediate', this.scan);
+				this.rows = new record_warehouse('Date');
+				this.table = $(this.table_selector);
+				this.control_row = $(this.control_row_selector);
+				$(this.new_control).on('click',
+						{
+							self: this,
+							call_for_rows: this.call_for_rows,
+							callForNewRow: this.callForNewRow,
+							control_row: this.control_row,
+							rows: this.rows,
+							row_html_template: this.row_html_template
+						}, this.new
+						);
+			},
+			this.new = function (e) {
+				if (e.data.self.call_for_rows) {
+					e.data.self.callForNewRow(e);
+					$(e.data.self.control_row).before(e.data.self.rows.add(e.data.self.row_html_template));
+				} else {
+					$(e.data.self.control_row).before(e.data.self.rows.add(e.data.self.row_html_template));
+				}
+			},
+			this.callForNewRow = function (e) {
+				$.ajax({
+					type: "GET",
+					dataType: "JSON",
+					url: e.data.self.new_call,
+					async: false,
+					success: function (data) {
+						e.data.self.row_html_template = data.row;
+					},
+					error: function (jqXHR, status, error) {
 
-	},
-	
-	/**
-	 * Overwrite the default properties with new values
-	 * 
-	 * @param {json object} config the values to substitute for the defaults
-	 */
-	this.configure = function(config) {
-		for (var p in config) {
-			this[p] = config[p];
-		}
-	}
-	
+					}
+				})
+
+			},
+			/**
+			 * Overwrite the default properties with new values
+			 * 
+			 * @param {json object} config the values to substitute for the defaults
+			 */
+			this.configure = function (config) {
+				for (var p in config) {
+					this[p] = config[p];
+				}
+			}
+
 	this.init(config);
-	
-};
+
+}
+;
 
 // ========================================================================================
 // ========================================================================================
@@ -125,8 +118,8 @@ function dt(config) {
  * unique for the group. xxxxx will be a the date in seconds with microseconds
  * 
  */
-function record_warehouse (model) {
-	
+function record_warehouse(model) {
+
 	this.model = model;
 	this.html_reference = {};
 	this.fields = {};
@@ -135,51 +128,49 @@ function record_warehouse (model) {
 	/**
 	 * Add a new htlm fragment to the stack
 	 * 
-	 * 
-	 * 
 	 * @param {html} fragment
 	 * @returns {Array} this last inserted element
 	 */
-	this.add = function(fragment){
-		
+	this.add = function (fragment) {
+
 		fragment_id = this.newFragmentId();
 		this.html_reference[fragment_id] = $(fragment);
 		this.fields[fragment_id] = {};
 		self = this;
 		parseFragment = this.parseFragment;
 		// uniquify the wrapper element
-		$(this.lastReference()).attr('id', this.idAttribute(this.lastReference())+'-'+fragment_id);
+		$(this.lastReference()).attr('id', this.idAttribute(this.lastReference()) + '-' + fragment_id);
 
 		// uniquify all the descendents that have IDs
 		// and store references to the fields
-		$(this.lastReference()).find('*').each(function(){
+		$(this.lastReference()).find('[id]').each(function () {
 			self.parseFragment(self, this);
 		});
-		
+
 		$(document).trigger('mediate', $(this.lastReference()));
 		return this.lastReference();
 	}
-	
+
 	/**
 	 * Return the last stored html fragment
 	 * 
 	 * @returns {Array} 
 	 */
-	this.lastReference = function() {
+	this.lastReference = function () {
 		if (this.fragment_id) {
 			return this.html_reference[this.fragment_id];
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Return the element's id if it exists or false
 	 * 
 	 * @param {html} element
 	 * @returns {String|Boolean}
 	 */
-	this.idAttribute = function(element){
-		if (typeof($(element).attr('id')) !== 'undefined') {
+	this.idAttribute = function (element) {
+		if (typeof ($(element).attr('id')) !== 'undefined') {
 			return $(element).attr('id');
 		} else {
 			return false;
@@ -192,8 +183,8 @@ function record_warehouse (model) {
 	 * @param {html} element
 	 * @returns {String|Boolean}
 	 */
-	this.fieldName = function(element){
-		if (typeof($(element).attr('name')) == 'undefined') {
+	this.fieldName = function (element) {
+		if (typeof ($(element).attr('name')) == 'undefined') {
 			return false;
 		}
 		var match = $(element).attr('name').match(/\[([a-z_]*)\]$/);
@@ -210,16 +201,16 @@ function record_warehouse (model) {
 	 * 
 	 * @returns {String}
 	 */
-	this.newFragmentId = function() {
-		this.fragment_id = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-});
+	this.newFragmentId = function () {
+		this.fragment_id = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
 //		this.fragment_id = new Date().getTime().toString();
 		return this.fragment_id;
 	}
-	
-	this.parseFragment = function(self, fragment) {
+
+	this.parseFragment = function (self, fragment) {
 		if (self.idAttribute(fragment)) {
 			$(fragment).attr('id', self.idAttribute(fragment) + '-' + self.fragment_id);
 			field = self.fieldName(fragment);
@@ -227,71 +218,73 @@ function record_warehouse (model) {
 				self.fields[self.fragment_id][field] = new Field($(fragment));
 			}
 		}
-	}		
-
-};
-	function Field(node) {
-		this.node = node;
-		var _id = false; // this is a private property
 	}
-	
-	// The id tools need to go on another object since they can be on any node, not just fields
-	// they are not bullet proof. though _id is now private, if the elements id attribute 
-	// gets changed by some other process, it won't be know by this class and we'll 
-	// keep getting back results based on the value whe the class first parsed it
-	Field.prototype = {
+
+}
+;
+function Field(node) {
+	NodeId.call(this, node);
+}
+
+Field.prototype = {
 //		_id: false,
-		get name() { return $(this.node).attr('name'); },
-		get value() { return $(this.node).val(); },
-		set value(newValue) { $(this.node).val(newValue); },
-		get fullId() {
-			if (!this._id) { this.parseId(); }
-			return this._id[0];
-		},
-		get baseId() {
-			if (!this._id) { this.parseId(); }
-			return this._id[1];
-		},
-		get uuId() {
-			if (!this._id) { this.parseId(); }
-			return this._id[2];
-		},
-		set uuId(uuid) {
-			this.node.attr('id', this.baseId() + '-' + uuid);
-		},
-		get alias() {
-			return this.name.match(/\[{1}([A-Z]{1}[a-z]+[A-Za-z]*)\]{1}/)[1];
-		},
-		parseId: function() {
-			this._id = $(this.node).attr('id').match(/([\w_]+)-*([a-f0-9]*)/);
-		}
-	}
-	
-	
+	get name() {
+		return $(this.node).attr('name');
+	},
+	get value() {
+		return $(this.node).val();
+	},
+	set value(newValue) {
+		$(this.node).val(newValue);
+	},
+	get alias() {
+		return this.name.match(/\[{1}([A-Z]{1}[a-z]+[A-Za-z]*)\]{1}/)[1];
+	},
+}
 
-function CakeTable_v2_6() {
-	
-//	this.table = new this.Table();
-	this.record = new Record();
-//	this.field = new this.Field();
-		
-	function Table(){
-		
-	}
-	function Record(){
-		
-		this.uuid = false;
-		 
-		this.newUuid = function() {
-//			this.uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			this.uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-				var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-				return v.toString(16);
-			});
-		return this.uuid;
-		}
-		
-	}
-	
+function  NodeId(node) {
+	this._id = false;
+	this.node = node;
+}
 
+NodeId.prototype = {
+	get fullId() {
+		if (!this._id) {
+			this.parseId();
+		}
+		return this._id[0];
+	},
+	get baseId() {
+		if (!this._id) {
+			this.parseId();
+		}
+		return this._id[1];
+	},
+	get uuId() {
+		if (!this._id) {
+			this.parseId();
+		}
+		return this._id[2];
+	},
+	set uuId(uuid) {
+		this.node.attr('id', this.baseId() + '-' + uuid);
+	},
+	parseId: function () {
+		this._id = $(this.node).attr('id').match(/([\w_]+)-*([a-f0-9]*)/);
+	}
+}
+
+Field.prototype = Object.create(NodeId.prototype);
+
+function Collection () {
+	
+}
+
+function mixin(receiver, supplier) {
+	Object.keys(supplier).forEach(function (property) {
+		var descriptor = Object.getOwnPropertyDescriptor(supplier, property);
+		Object.defineProperty(receiver, property, descriptor);
+	});
+
+	return receiver;
 }
