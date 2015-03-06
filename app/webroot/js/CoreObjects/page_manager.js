dbc5689ff89ef9df43b9435748bb8c260f440c58
Don Drake <ddrake@dreamingmind.com>
2015-03-05 21:40:47 -0800

    build up new code for parsing. untested

--- app/webroot/js/CoreObjects/page_manager.js
+++ app/webroot/js/CoreObjects/page_manager.js
@@ -1,9 +1,19 @@
 function PageManager() {
 	
-	this.field = [];
+	this.fields = [];
+	this.fragments = [];
+	this.uuid
 	
+	/**
+	 * Add more ID'd elements to the lookup tables
+	 * 
+	 * @param {Element} node
+	 */
 	this.add = function(node) {
-		this.field.push(new Field(node));
+//		this.field.push(new Field(node));
+		$($(node).wrap('div')).find('[id]').each(function () {
+			parseFragment.call(PageManager.rows, this);
+		});
 	}
 	
 	/**
@@ -16,44 +26,71 @@ function PageManager() {
 			return v.toString(16);
 		});
 	}
+	
+	/**
+	 * Fold nodes into the lookup structures
+	 * 
+	 * Only elements with ID attributes come here. 
+	 * The IDs get a uuid appended so fields an such can be 
+	 * gathered together in groupings (eg. records) 
+	 * 
+	 * BUSINESS RULE:
+	 * TR and FIELDSET will start a new uuid grouping, 
+	 * all other elements use the current uuid if it exists 
+	 * and will set one if it doesn't exist.
+	 * 
+	 * @param {Element} fragment
+	 */
+	this.parseFragment = function (fragment) {
+		switch (fragment.tagName) {
+			case 'INPUT':
+			case 'SELECT':
+			case 'TEXTAREA':
+				var node = new Field($(fragment));
+				break;
+			case 'TR':
+			case 'FIELDSET':
+				this.uuid = this.newUuid();
+			case 'BUTTON':
+			case 'FORM':
+			default :
+				var node = new ManagedNode($(fragment));
+				break;
+		}
+		node.uuId = this.newUuid();
+		if (Field.isPrototypeOf(node)) {
+			this.storeField(node);
+		} else if (ManagedNode.isPrototypeOf(node)) {
+			this.storeFragment(node);
+		}
+	}
 
+	/**
+	 * Store a Field object in the lookup structure(s)
+	 * 
+	 * @param {Field} node
+	 */
+	this.storeField = function(node) {
+		if (this.fields[node.uuId] === 'undefined') {
+			this.fields[node.uuId] = [];
+		}				
+		this.fields[node.uuId][node.name] = node;
+	}
+	
+	/**
+	 * Store an ID'd node in the lookup structure(s)
+	 * 
+	 * @param {ManagedNode} node
+	 */
+	this.storeFragment = function(node) {
+		if (this.fragments[node.uuId] === 'undefined') {
+			this.fragments[node.uuId] = [];
+		}				
+		this.fragments[node.uuid].push(node);
+	}				
+	
 } // END OF PAGE MANAGER CLASS
 
-function Field() {
-	ManagedNode.call(this);
-//	this._id = false;
-//	this.node = 'node';
-}
-
-Field.prototype = Object.create(ManagedNode.prototype);
-
-
-function viewprop(obj) {
-	for (var prop in obj) {
-		alert('<p class="properties">' + prop + ' : ' + obj[prop] + '</p>');
-	}
-};
-
-viewprop(Field);
-
-
-Field.prototype = {
-	constructor: Field,
-//		_id: false,
-	get name() {
-		return $(this.node).attr('name');
-	},
-	get value() {
-		return $(this.node).val();
-	},
-	set value(newValue) {
-		$(this.node).val(newValue);
-	},
-	get alias() {
-		return this.name.match(/\[{1}([A-Z]{1}[a-z]+[A-Za-z]*)\]{1}/)[1];
-	},
-}
-viewprop(Field);
 
 function  ManagedNode() {
 	this._id = false;
@@ -87,5 +124,46 @@ ManagedNode.prototype = {
 		this._id = $(this.node).attr('id').match(/([\w_]+)-*([a-f0-9]*)/);
 	}
 }
+
+function Field() {
+	ManagedNode.call(this);
+}
+
+Field.prototype = Object.create(ManagedNode.prototype, {
+		constructor: {
+			configurable: true,
+			enumerable: true,
+			value: Field,
+			writable: true
+		}
+	});
+
+Object.defineProperty(Field.prototype, "name", {
+    get: function() {
+		return $(this.node).attr('name');
+    },
+    enumerable: true,
+    configurable: true
+});
+
+Object.defineProperty(Field.prototype, "alias", {
+    get: function() {
+		return this.name.match(/\[{1}([A-Z]{1}[a-z]+[A-Za-z]*)\]{1}/)[1];
+    },
+    enumerable: true,
+    configurable: true
+});
+
+Object.defineProperty(Field.prototype, "value", {
+    get: function() {
+		return $(this.node).val();
+    },
+    set: function(newValue) {
+		$(this.node).val(newValue);
+    },
+    enumerable: true,
+    configurable: true
+});
+
 
 
